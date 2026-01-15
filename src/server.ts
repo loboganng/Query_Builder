6e059821dc9c7351a1b811ec8863e6288e29bc03
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-15 19:36:06 -0300

    Have finished creating our API, adding UPDATE and DELETE methods to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,46 @@
 import express, { Request, Response } from "express"
 import { knex } from "./database/knex"
+import { request } from "http"
 
 const app = express()
 app.use(express.json())
 
-// app.get("/", async (request: Request, response: Response) => {
-//   response.json({ message: "Hello World!" })
-// })
+app.get("/courses", async (request: Request, response: Response) => {
+  //RAW method
+  // const courses = await knex.raw("SELECT * FROM courses")
+
+  //Knex method
+  const courses = await knex("courses").select().orderBy("name", "desc")
+
+  return response.json(courses)
+})
 
 app.post("/courses", async (request: Request, response: Response) => {
   const { name } = request.body
 
-  //Here we're able to insert data into the database, using knex
+  //Knex method
   // await knex("courses").insert({ name })
 
-  //Now we'll utilise the raw method to input actual SQL code into our program
-  //This can also be used for any other method
+  //RAW method
   await knex.raw("INSERT INTO courses (name) VALUES (?)", [name])
 
-  response.status(201).json()
+  return response.status(201).json()
+})
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } =  request.params
+  const { name } = request.body
+
+  await knex("courses").update({ name }).where({ id })
+
+  return response.json()
+})
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params
+
+  await knex("courses").delete().where({ id })
+  return response.json()
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
